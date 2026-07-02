287d001d52885d4cb6e3fcfa35565034ec997858
Edos24 <treyper24@gmail.com>
2026-07-02 14:30:44 -0600

    Cambio_de_la_carpeta_raiz_del_Proyecto

--- Rulera_Mundialera/Rulera_Mundialera.Server/obj/Debug/net10.0/Rulera_Mundialera.Server.AssemblyInfo.cs
+++ Rulera_Mundialera/Rulera_Mundialera.Server/obj/Debug/net10.0/Rulera_Mundialera.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rulera_Mundialera.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf4ba6fd8b82247a28187709c67116060bca279b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rulera_Mundialera.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rulera_Mundialera.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
