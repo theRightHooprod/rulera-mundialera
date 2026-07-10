8b01442587f8ec500ab9795551e8f481367dff04
Edos24 <treyper24@gmail.com>
2026-07-10 13:19:30 -0600

    Creacion de las carpetas para el Test de la tabla

---
+++ rulera-mundialera.Server/Database/Database.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using rulera_mundialera.Server.Database.Entities;
+
+namespace rulera_mundialera.Server.Database
+{
+    public class Database : DbContext
+    {
+        public DbSet<Test> tests { get; set; }
+
+        public Database()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+       => optionsBuilder.UseNpgsql("Host=localhost;Database=rulera-mundialeradb;Username=psql;Password=D1rrhb6BdE24");
+
+
+    }
+}
---
+++ rulera-mundialera.Server/Database/Entities/Test.cs
@@ -0,0 +1,7 @@
+﻿namespace rulera_mundialera.Server.Database.Entities
+{
+    public class Test
+    {
+        public int nombre {  get; set; }
+    }
+}
